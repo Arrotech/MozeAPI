4d5f627662059a36914781739c9b4766e7d4e2c3
Arrotech <arrotechdesign@gmail.com>
2019-08-08 00:09:08 +0300

    Feature (Create Account) A user can create account
    What does this pr do?
    
    Have a new user able to create a new account.
    
    Any tasks involved?
    
    Create a database connection.
    Create registration endpoint.
    Setup virtual environment.
    Install dependencies.
    Validate registration endpoint.
    Write unit tests for the endpoint.
    
    Any related pivotal stories?
    
    [Finishes #167770989]

---
+++ run.py
@@ -0,0 +1,24 @@
+import os
+
+from app import moze_app
+from flask_jwt_extended import JWTManager
+from app.api.v1.models.database import Database
+
+config_name = os.getenv('APP_SETTINGS')
+app = moze_app(config_name)
+
+
+@app.cli.command()
+def create():
+    """Create tables."""
+
+    Database().create_table()
+
+@app.cli.command()
+def destroy():
+    """Destroy tables."""
+
+    Database().destroy_table()
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ utils/v1/dummy.py
@@ -0,0 +1,197 @@
+# @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ Auth Information @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+new_account = {
+    "firstname": "Harun",
+    "lastname": "Gachanja",
+    "surname": "Gitundu",
+    "phone": "0711371265",
+    "location": "Nakuru, Njoro",
+    "portfolio": "Education",
+    "occupation": "carpenter",
+    "username": "Arrotech",
+    "email": "harungachanjagitundu@gmail.com",
+    "password": "Harun20930988!",
+    "cost": 2500.50
+}
+
+wrong_account_keys = {
+    "firstname": "Harun",
+    "lastname": "Gachanja",
+    "surname": "Gitundu",
+    "phone": "0711371265",
+    "location": "Nakuru, Njoro",
+    "portfolio": "Education",
+    "occupation": "carpenter",
+    "usname": "Arrotech",
+    "email": "harungachanjagitundu@gmail.com",
+    "password": "Harun20930988!",
+    "cost": 2500.50
+}
+
+wrong_account_firstname = {
+    "firstname": "Ha  run",
+    "lastname": "Gachanja",
+    "surname": "Gitundu",
+    "phone": "0711371265",
+    "location": "Nakuru, Njoro",
+    "portfolio": "Education",
+    "occupation": "carpenter",
+    "username": "Arrotech",
+    "email": "harungachanjagitundu@gmail.com",
+    "password": "Harun20930988!",
+    "cost": 2500.50
+}
+
+wrong_account_lastname = {
+    "firstname": "Harun",
+    "lastname": "Gacha nja",
+    "surname": "Gitundu",
+    "phone": "0711371265",
+    "location": "Nakuru, Njoro",
+    "portfolio": "Education",
+    "occupation": "carpenter",
+    "username": "Arrotech",
+    "email": "harungachanjagitundu@gmail.com",
+    "password": "Harun20930988!",
+    "cost": 2500.50
+}
+
+wrong_account_surname = {
+    "firstname": "Harun",
+    "lastname": "Gachanja",
+    "surname": "Git786undu",
+    "phone": "0711371265",
+    "location": "Nakuru, Njoro",
+    "portfolio": "Education",
+    "occupation": "carpenter",
+    "username": "Arrotech",
+    "email": "harungachanjagitundu@gmail.com",
+    "password": "Harun20930988!",
+    "cost": 2500.50
+}
+
+wrong_account_portfolio = {
+    "firstname": "Harun",
+    "lastname": "Gachanja",
+    "surname": "Gitundu",
+    "phone": "0711371265",
+    "location": "Nakuru, Njoro",
+    "portfolio": "Ed  ation",
+    "occupation": "carpenter",
+    "username": "Arrotech",
+    "email": "harungachanjagitundu@gmail.com",
+    "password": "Harun20930988!",
+    "cost": 2500.50
+}
+
+wrong_account_occupation = {
+    "firstname": "Harun",
+    "lastname": "Gachanja",
+    "surname": "Gitundu",
+    "phone": "0711371265",
+    "location": "Nakuru, Njoro",
+    "portfolio": "Education",
+    "occupation": "carp  enter",
+    "username": "Arrotech",
+    "email": "harungachanjagitundu@gmail.com",
+    "password": "Harun20930988!",
+    "cost": 2500.50
+}
+
+wrong_account_phone = {
+    "firstname": "Harun",
+    "lastname": "Gachanja",
+    "surname": "Gitundu",
+    "phone": "6711371265",
+    "location": "Nakuru, Njoro",
+    "portfolio": "Education",
+    "occupation": "carpenter",
+    "username": "Arrotech",
+    "email": "harungachanjagitundu@gmail.com",
+    "password": "Harun20930988!",
+    "cost": 2500.50
+}
+
+wrong_account_email = {
+    "firstname": "Harun",
+    "lastname": "Gachanja",
+    "surname": "Gitundu",
+    "phone": "0711371265",
+    "location": "Nakuru, Njoro",
+    "portfolio": "Education",
+    "occupation": "carpenter",
+    "username": "Arrotech",
+    "email": "harungachanjagitundugmailcom",
+    "password": "Harun20930988!",
+    "cost": 2500.50
+}
+
+wrong_account_password = {
+    "firstname": "Harun",
+    "lastname": "Gachanja",
+    "surname": "Gitundu",
+    "phone": "0711371265",
+    "location": "Nakuru, Njoro",
+    "portfolio": "Education",
+    "occupation": "carpenter",
+    "username": "Arrotech",
+    "email": "harungachanjagitundu@gmail.com",
+    "password": "Harun20930988",
+    "cost": 2500.50
+}
+
+wrong_account_cost = {
+    "firstname": "Harun",
+    "lastname": "Gachanja",
+    "surname": "Gitundu",
+    "phone": "0711371265",
+    "location": "Nakuru, Njoro",
+    "portfolio": "Education",
+    "occupation": "carpenter",
+    "username": "Arrotech",
+    "email": "harungachanjagitundu@gmail.com",
+    "password": "Harun20930988!",
+    "cost": "2500.50"
+}
+
+phone_exists = {
+    "firstname": "Harun",
+    "lastname": "Gachanja",
+    "surname": "Gitundu",
+    "phone": "0711371265",
+    "location": "Nakuru, Njoro",
+    "portfolio": "Education",
+    "occupation": "carpenter",
+    "username": "Arrotech254",
+    "email": "gitundu@gmail.com",
+    "password": "Harun20930988!",
+    "cost": 2500.50
+}
+
+username_exists = {
+    "firstname": "Harun",
+    "lastname": "Gachanja",
+    "surname": "Gitundu",
+    "phone": "0797568564",
+    "location": "Nakuru, Njoro",
+    "portfolio": "Education",
+    "occupation": "carpenter",
+    "username": "Arrotech",
+    "email": "gitundugachanja94@gmail.com",
+    "password": "Harun20930988!",
+    "cost": 2500.50
+}
+
+email_exists = {
+    "firstname": "Harun",
+    "lastname": "Gachanja",
+    "surname": "Gitundu",
+    "phone": "0711853324",
+    "location": "Nakuru, Njoro",
+    "portfolio": "Education",
+    "occupation": "carpenter",
+    "username": "Arrotech2547",
+    "email": "harungachanjagitundu@gmail.com",
+    "password": "Harun20930988!",
+    "cost": 2500.50
+}
