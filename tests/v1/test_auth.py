693dbe4257207fef944433d298ad8a76841da0a1
Arrotech <arrotechdesign@gmail.com>
2019-08-09 10:28:34 +0300

    Feature (Refresh Token) User can refresh a token
    What does this pr do?
    
    Have a user able to refresh a token.
    
    Any tasks involved?
    
    Create refresh token endpoint.
    Write unit test to test the endpoint.
    
    Any related pivotal stories?
    
    [Finishes #167803476]

--- tests/v1/test_auth.py
+++ tests/v1/test_auth.py
@@ -179,3 +179,11 @@ class TestUsersAccount(BaseTest):
         result = json.loads(response1.data.decode())
         self.assertEqual(result['message'], 'Invalid Email or Password')
         assert response1.status_code == 401
+
+    def test_refresh_token(self):
+        """Test the vote json keys."""
+
+        response = self.client.post(
+            '/api/v1/auth/refresh', content_type='application/json',
+            headers=self.get_refresh_token())
+        assert response.status_code == 200
