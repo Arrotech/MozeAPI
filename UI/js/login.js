fe2013614bdb18144ecd020256e4a4bbba1bfd36
Arrotech <arrotechdesign@gmail.com>
2019-08-30 14:09:37 +0300

    Feature (Login) Fetch login
    What does this pr do?
    
    Have a registered user able to login.
    
    Any tasks involved?
    
    Create login page.
    Fecth login api.
    
    Any related pivotal stories?
    
    [Finishes #168216333]

---
+++ UI/js/login.js
@@ -0,0 +1,56 @@
+function callToast() {
+    var x = document.getElementById("snackbar");
+    x.className = "show";
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+}
+function onSuccess(msg){
+    document.getElementById('snackbar').innerText = msg
+    callToast();
+}
+function raiseError(msg){
+    document.getElementById('snackbar').innerText = msg
+    callToast();
+}
+
+document.getElementById('login').onclick = () => {
+        event.preventDefault();
+        let email = document.getElementById('email').value;
+        let password = document.getElementById('password').value;
+        fetch('http://localhost:5000/api/v1/auth/login', {
+            method: 'POST',
+            headers : {
+            Accept: 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body:JSON.stringify({email:email, password:password})
+        }).then((res) => res.json())
+        .then((data) =>  {
+            console.log(data);
+            let user = data['user'];
+            let status = data['status'];
+            let message = data['message'];
+            if (status === '200'){
+              if (email === 'admin@admin.com'){
+                  localStorage.setItem('token', data.token);
+                  localStorage.setItem('user', data.user);
+                  localStorage.setItem('username', data.user.username);
+                  localStorage.setItem('email', data.user.email);
+                  onSuccess('Signed in successfully!');
+                  window.location.replace('admin.html');
+              }else {
+                  localStorage.setItem('token', data.token);
+                  localStorage.setItem('user', data.user);
+                  localStorage.setItem('username', data.user.username);
+                  localStorage.setItem('email', data.user.email);
+                  onSuccess('Signed in successfully!');
+                  window.location.replace('user.html');
+              }
+            }else{
+                raiseError(message);
+            }
+        })
+        .catch((err)=> {
+            raiseError("Please check your internet connection!");
+            console.log(err);
+        })
+    }
